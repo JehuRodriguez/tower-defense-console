a8164afa3a951042cda49504771ee8b965066600
Jehu Rodriguez <j.eh.u14@hotmail.com>
2026-09-25 00:33:54 -0500

    Agregando ArcherTower y CannonTower(herencia y polimorfismo)

---
+++ TowerDefenseConsole/Models/ArcherTower.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+
+namespace TowerDefenseConsole.Models
+{
+    public class ArcherTower : Tower
+    {
+        public ArcherTower() : base("Torre Arquero", 15, 3, 50)
+        {
+        }
+
+
+        public override void Atacar(Enemy objetivo)
+        {
+            Console.WriteLine($"{Nombre} dispara una flecha veloz a {objetivo.Nombre}!");
+            objetivo.RecibirDaño(Daño);
+        }
+    }
+}
---
+++ TowerDefenseConsole/Models/CannonTower.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace TowerDefenseConsole.Models
+{
+    public class CannonTower : Tower
+    {
+        public CannonTower() : base("Torre Cañón", 40, 2, 100)
+        {
+        }
+
+        public override void Atacar(Enemy objetivo)
+        {
+            Console.WriteLine($"{Nombre} dispara una bala de cañón explosiva a {objetivo.Nombre}!");
+            objetivo.RecibirDaño(Daño);
+        }
+    }
+}
