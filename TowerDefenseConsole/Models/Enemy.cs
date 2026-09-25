439fbae1e92ff3991102bb63546f7749902cf4ef
Jehu Rodriguez <j.eh.u14@hotmail.com>
2026-09-25 00:14:10 -0500

    Agrego clase Enemy y corrijo referneica en IAtacante

---
+++ TowerDefenseConsole/Models/Enemy.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using TowerDefenseConsole.Interfaces;
+
+
+namespace TowerDefenseConsole.Models
+{
+    public class Enemy :IMovible
+    {
+        public string Nombre { get; set; }
+        public int Vida { get; set; }
+        public int Posicion { get; set; }
+        public int Velocidad { get; set; }
+
+        public Enemy(string nombre, int vida, int velocidad)
+        {
+            Nombre = nombre;
+            Vida = vida;
+            Velocidad = velocidad;
+            Posicion = 0;
+        }
+
+        public void Mover()
+        {
+            Posicion += Velocidad;
+            Console.WriteLine($"{Nombre} avanza hasta la posición {Posicion}");
+        }
+
+        public void RecibirDaño(int daño)
+        {
+            Vida -= daño;
+            if (Vida < 0) Vida = 0;
+            Console.WriteLine($"{Nombre} recibió {daño} de daño. Vida restante: {Vida}");
+        }
+
+        public bool EstaVivo()
+        {
+            return Vida > 0;
+        }
+    }
+}
