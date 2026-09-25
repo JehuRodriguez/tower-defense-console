39d8a1f892841b503d799b02ab0e1308f1410c6a
Jehu Rodriguez <j.eh.u14@hotmail.com>
2026-09-25 00:20:12 -0500

    Agrego clase abstracta TOWER

---
+++ TowerDefenseConsole/Models/Tower.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using TowerDefenseConsole.Interfaces;
+
+namespace TowerDefenseConsole.Models
+{
+    public abstract class Tower : IAtacante
+    {
+        public string Nombre { get; set; }
+        public int Daño { get; set; }
+        public int Rango { get; set; }
+        public int Costo { get; set; }
+
+        public Tower(string nombre, int daño, int rango, int costo)
+        {
+            Nombre = nombre;
+            Daño = daño;
+            Rango = rango;
+            Costo = costo;
+        }
+
+        public abstract void Atacar(Enemy objetivo);
+
+        public void MostrarInfo()
+        {
+            Console.WriteLine($"Torre: {Nombre} | Daño: {Daño} | Rango: {Rango} | Costo: {Costo}");
+        }
+    }
+}
