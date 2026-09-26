77b7986f222a451f3461403963f5ae93a0028978
Jehu Rodriguez <j.eh.u14@hotmail.com>
2026-09-26 05:32:21 -0500

    Agrego implementacion de Fibonacci para escalar nivel y daño de las torres

--- TowerDefenseConsole/Models/Tower.cs
+++ TowerDefenseConsole/Models/Tower.cs
@@ -9,6 +9,7 @@ namespace TowerDefenseConsole.Models
         public int Daño { get; set; }
         public int Rango { get; set; }
         public int Costo { get; set; }
+        public int Nivel { get; set; } = 1;
 
         public Tower(string nombre, int daño, int rango, int costo)
         {
@@ -22,7 +23,35 @@ namespace TowerDefenseConsole.Models
 
         public void MostrarInfo()
         {
-            Console.WriteLine($"Torre: {Nombre} | Daño: {Daño} | Rango: {Rango} | Costo: {Costo}");
+            Console.WriteLine("Torre: " + Nombre + "| Daño:" + Daño + "| Rango: " + Rango + "| Costo: " + Costo + "| Nivel: " + Nivel);
+        }
+
+        public int CalcularFibonacci(int n)
+        {
+            if (n <= 1)
+                return n;
+
+            int anterior = 0;
+            int actual = 1;
+
+            for(int i= 2; i<=n; i++)
+            {
+                int siguiente = anterior + actual;
+
+                anterior = actual;
+                actual = siguiente;
+            }
+            return actual;
+        }
+
+        public void SubirNivel()
+        {
+            Nivel++;
+            int multiplicadorFibonacci = CalcularFibonacci(Nivel);
+            Daño = Daño + multiplicadorFibonacci;
+            Console.WriteLine(Nombre + "subio a nivel " + Nivel + "! Nuevo daño: " + Daño + "(bonus Fibonacci : " + multiplicadorFibonacci + ")");
+
+
         }
     }
 }
